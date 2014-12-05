914befdd96fff4aa742a84a41de85a1dd3423255
Davtux <barrat.romain87@gmail.com>
2014-12-05 13:49:18 +0000

    Exercice 1

--- TD2Thread/src/exo1/adapteur/FileImpl.java
+++ TD2Thread/src/exo1/adapteur/FileImpl.java
@@ -1,43 +1,46 @@
 package exo1.adapteur;
 
+import java.nio.channels.IllegalSelectorException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FileImpl<E> implements File<E> {
 	private final List<E> liste;
-	
+
 	public FileImpl() {
 		this.liste = new ArrayList<E>();
 	}
 
 	@Override
 	public E tete() {
-		if(!this.estVide())
-			return this.get(0);
+		if (!this.estVide())
+			return liste.get(0);
 		return null;
 	}
 
 	@Override
 	public void insererQueue(E e) {
-		// TODO Auto-generated method stub
-		
+		liste.add(e);
 	}
 
 	@Override
 	public E retirerTete() {
-		// TODO Auto-generated method stub
-		return null;
+		if (this.estVide()) {
+			throw new IllegalSelectorException();
+		}
+
+		return liste.remove(0);
 	}
 
 	@Override
 	public int longueur() {
-		// TODO Auto-generated method stub
-		return 0;
+		return liste.size();
 	}
 
 	@Override
 	public boolean estVide() {
-		// TODO Auto-generated method stub
+		if (liste.size() == 0)
+			return true;
 		return false;
 	}
 
--- TD2Thread/src/exo1/adapteur/TestFile.java
+++ TD2Thread/src/exo1/adapteur/TestFile.java
@@ -2,6 +2,10 @@ package exo1.adapteur;
 
 public class TestFile {
 	public static void main(String[] args) {
-		
+		final File<Integer> file = new FileImpl<Integer>();
+		file.insererQueue(Integer.valueOf(10));
+		file.insererQueue(Integer.valueOf(20));
+		file.retirerTete();
+		System.out.println(file.tete());
 	}
 }
