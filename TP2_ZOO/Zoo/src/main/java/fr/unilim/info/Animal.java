066351a074bfd304a8f8d4ea3e96c6361f546669
Davtux <barrat.romain87@gmail.com>
2014-12-11 18:55:57 +0000

    Javadoc TP JUnit.

--- TP2_ZOO/Zoo/src/main/java/fr/unilim/info/Animal.java
+++ TP2_ZOO/Zoo/src/main/java/fr/unilim/info/Animal.java
@@ -5,12 +5,27 @@ public class Animal {
 	private boolean carnivore;
 	private float poids;
 
+	/**
+	 * Constructeur d'animal.
+	 * 
+	 * @param espece
+	 *            : espèce de l'animal.
+	 * @param isCarnivore
+	 *            : détermine l'alimentation de l'animal.
+	 * @param poids
+	 *            : poids de l'animal.
+	 */
 	public Animal(String espece, boolean isCarnivore, float poids) {
 		this.espece = espece;
 		this.carnivore = isCarnivore;
 		this.poids = poids;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -21,12 +36,22 @@ public class Animal {
 		return result;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
 	@Override
 	public String toString() {
 		return "espece=[" + espece + "], isCarnivore=[" + carnivore
 				+ "], poids=[" + poids + "]";
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -48,30 +73,70 @@ public class Animal {
 		return true;
 	}
 
+	/**
+	 * Getter d'espèce.
+	 * 
+	 * @return l'espèce de l'animal.
+	 */
 	public String getEspece() {
 		return espece;
 	}
 
+	/**
+	 * Setter d'espèce.
+	 * 
+	 * @param espece
+	 *            : espèce de l'animal.
+	 */
 	public void setEspece(String espece) {
 		this.espece = espece;
 	}
 
+	/**
+	 * Getter : l'alimentation de l'animal.
+	 * 
+	 * @return un booléen : vrai si l'animal est carnivore faux sinon.
+	 */
 	public boolean isCarnivore() {
 		return carnivore;
 	}
 
+	/**
+	 * Setter : alimentation de l'animal.
+	 * 
+	 * @param isCarnivore
+	 *            : vrai si l'animal est carnivore faux sinon.
+	 */
 	public void setCarnivore(boolean isCarnivore) {
 		this.carnivore = isCarnivore;
 	}
 
+	/**
+	 * Getter poids : retourne le poids de l'animal.
+	 * 
+	 * @return float : poids de l'animal.
+	 */
 	public float getPoids() {
 		return poids;
 	}
 
+	/**
+	 * Setter poids : affecte un poids à l'animal.
+	 * 
+	 * @param poids
+	 *            : poids de l'animal.
+	 */
 	public void setPoids(float poids) {
 		this.poids = poids;
 	}
 
+	/**
+	 * Renvoie un booléen déterminant si il faut soigner l'animal.
+	 * 
+	 * @param typeSoin
+	 *            : DENTS ou PIEDS.
+	 * @return : booléen : vrai si il faut le soigner, faux sinon.
+	 */
 	public boolean necessiteSoin(SoinEnum typeSoin) {
 		if (typeSoin.equals(SoinEnum.DENTS) && this.isCarnivore()) {
 			return true;
@@ -82,6 +147,11 @@ public class Animal {
 		return false;
 	}
 
+	/**
+	 * Renvoie la quantité de viande à fournir à l'animal pour une semaine.
+	 * 
+	 * @return float : quantité de viande.
+	 */
 	public float calculerViandeHebdo() {
 		if (this.isCarnivore()) {
 			return this.getPoids() * 0.25f;
